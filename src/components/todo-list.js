966c26dbe09ae9530b30fb94a2cd5c0a9f8969c0
RustamKeneev <dr.rustamkeneev@gmail.com>
2022-04-24 18:53:10 +0600

    refactoring code -> array elements display with spread objects

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -4,7 +4,7 @@ import TodoListItem from './todo-list-item';
 const TodoList = ({todos}) => {
     const elements = todos.map((item)=> {
         return(
-        <li><TodoListItem label={item.label} important={item.important}/></li>
+        <li><TodoListItem {...item}/></li>
         )
     })
     return (
