7a6327bf639150cc39ec9b1ac1553ce16906eac1
RustamKeneev <dr.rustamkeneev@gmail.com>
2022-04-26 14:11:58 +0600

    update components

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -17,12 +17,18 @@ export default class ItemAddForm extends Component{
     onSubmit = (e) => {
         e.preventDefault()
         this.props.onItemAdded(this.state.label);
+        this.setState({
+            label:''
+        })
     }
+
     render() {
         return(
             <form className={"item-add-form d-flex"} onSubmit={this.onSubmit}>
                 <input type={"text"} className={"form-control"}
-                       onChange={this.onLabelChange} placeholder={"Whats needs to be done"}/>
+                       onChange={this.onLabelChange}
+                       placeholder={"Whats needs to be done"}
+                       value={this.state.label}/>
                 <button className={"btn btn-outline-secondary"}>
                     Add Item
                 </button>
